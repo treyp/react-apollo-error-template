93ed44afd4c2135cb804a5a2dbf53e9d7d97bfd3
Trey Philips <treyjp@gmail.com>
2019-08-14 00:40:31 -0500

    Demonstrate bug where fetchMore's updateQuery previousResults argument is undefined

--- src/App.js
+++ src/App.js
@@ -1,38 +1,121 @@
 import React from "react";
-import { useQuery } from "@apollo/react-hooks";
+import { graphql } from "react-apollo";
 import gql from "graphql-tag";
+import { isArray, flowRight as compose } from "lodash";
 
-const ALL_PEOPLE = gql`
-  query AllPeople {
-    people {
-      id
-      name
+const queryVariables = state => ({
+  limit: 3,
+  page: (state && state.page) || 1,
+  order: (state && state.order) || "asc",
+  explode: (state && state.explode) || false
+});
+
+class People extends React.PureComponent {
+  state = {
+    page: 1,
+    order: "asc",
+    explode: false
+  };
+
+  handleExplode = e => {
+    if (e.target.checked !== this.state.explode) {
+      this.setState({ explode: !!e.target.checked });
     }
   }
-`;
 
-export default function App() {
-  const {
-    loading,
-    data: { people }
-  } = useQuery(ALL_PEOPLE);
+  handleSort = e => {
+    const nextOrder = e.target.checked ? "asc" : "desc";
+    const prevOrder = this.state.order;
+    if (prevOrder === nextOrder) {
+      return;
+    }
+    this.props.people.refetch(
+      queryVariables({ ...this.state, order: nextOrder, page: 1 })
+    ).then(() => {
+      this.setState({ order: nextOrder, page: 1 });
+    }).catch(() => {
+      console.log("Sort failed");
+    });
+  };
 
-  return (
-    <main>
-      <h1>Apollo Client Issue Reproduction</h1>
-      <p>
-        This application can be used to demonstrate an error in Apollo Client.
-      </p>
-      <h2>Names</h2>
-      {loading ? (
-        <p>Loading…</p>
-      ) : (
-        <ul>
-          {people.map(person => (
-            <li key={person.id}>{person.name}</li>
-          ))}
-        </ul>
-      )}
-    </main>
-  );
+  addPage = () => {
+    const nextPage = this.state.page + 1;
+
+    this.props.people.fetchMore({
+      variables: queryVariables({ ...this.state, page: nextPage }),
+      updateQuery: (previousResults, { fetchMoreResult }) => {
+        console.log("updateQuery called with previousResults: ", previousResults);
+
+        if (!fetchMoreResult || !isArray(fetchMoreResult.people)) {
+          return previousResults;
+        }
+
+        return {
+          ...fetchMoreResult,
+          people: previousResults.people.concat(fetchMoreResult.people)
+        };
+      }
+    })
+    .then(() => {
+      this.setState({
+        page: nextPage
+      });
+    })
+    .catch(error => {
+      console.log("Pagination failed with error:", error);
+    });
+  };
+
+  render() {
+    console.log("Component rendering with data:", this.props.people);
+    // console.log("State during render:", this.state);
+
+    const {
+      loading,
+      people
+    } = this.props.people;
+
+    return (
+      <main>
+        <h1>Apollo Client Issue Reproduction</h1>
+        <div>
+          <label>
+            <input type="checkbox" checked={this.state.explode} onChange={this.handleExplode} /> Offline?
+            <span role="img" aria-label="boom">💥</span>
+          </label>
+        </div>
+        <h2>Names</h2>
+        {loading ? (
+          <p>Loading…</p>
+        ) : (
+            <ul>
+              {people.map(person => (
+                <li key={person.id}>{person.name}</li>
+              ))}
+              <li><button onClick={this.addPage}>Load more</button></li>
+            </ul>
+          )}
+        <div><label><input type="checkbox" checked={this.state.order === "asc"} onChange={this.handleSort} /> Sort in ascending order</label></div>
+      </main>
+    );
+  }
 }
+
+const PeopleWithData = compose(
+  graphql(
+    gql`
+      query SomePeople($order: String!, $limit: Int!, $page: Int!, $explode: Boolean) {
+        people(order: $order, limit: $limit, page: $page, explode: $explode) {
+          id
+          name
+        }
+      }
+    `,
+    {
+      name: "people",
+      options: props => ({ variables: queryVariables() })
+    }
+  )
+)(People);
+
+export default PeopleWithData;
--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -3,31 +3,53 @@ import {
   GraphQLObjectType,
   GraphQLID,
   GraphQLString,
-  GraphQLList,
-} from 'graphql';
+  GraphQLInt,
+  GraphQLBoolean,
+  GraphQLList
+} from "graphql";
 
 const PersonType = new GraphQLObjectType({
-  name: 'Person',
+  name: "Person",
   fields: {
     id: { type: GraphQLID },
-    name: { type: GraphQLString },
-  },
+    name: { type: GraphQLString }
+  }
 });
 
-const peopleData = [
-  { id: 1, name: 'John Smith' },
-  { id: 2, name: 'Sara Smith' },
-  { id: 3, name: 'Budd Deey' },
-];
+const peopleData = [];
+for (let pdIndex = 0; pdIndex < 26; pdIndex++) {
+  peopleData.push({id: pdIndex, name: String.fromCharCode("A".charCodeAt(0) + pdIndex)})
+}
 
 const QueryType = new GraphQLObjectType({
-  name: 'Query',
+  name: "Query",
   fields: {
     people: {
       type: new GraphQLList(PersonType),
-      resolve: () => peopleData,
-    },
-  },
+      args: {
+        order: {type: GraphQLString},
+        limit: {type: GraphQLInt},
+        page: {type: GraphQLInt},
+        explode: {type: GraphQLBoolean}
+      },
+      resolve: (_, args) => {
+        if (args.explode) {
+          throw new Error("Server \"down!\"");
+        }
+        const start = (args.page - 1) * args.limit;
+        const end = start + args.limit;
+        return [...peopleData].sort((a, b) => {
+          if (a.name === b.name) {
+            return 0;
+          }
+          if (a.name > b.name) {
+            return args.order === "asc" ? 1 : -1;
+          }
+          return args.order === "asc" ? -1 : 1;
+        }).slice(start, end);
+      }
+    }
+  }
 });
 
 export const schema = new GraphQLSchema({ query: QueryType });
