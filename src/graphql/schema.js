5024c4116e628e4f4195e52233c2560d4d61a20a
Trey Philips <treyjp@gmail.com>
2021-05-25 22:28:15 -0500

    Issue #5169 works when using hooks

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -4,6 +4,8 @@ import {
   GraphQLID,
   GraphQLString,
   GraphQLList,
+  GraphQLInt,
+  GraphQLBoolean,
 } from 'graphql';
 
 const PersonType = new GraphQLObjectType({
@@ -14,18 +16,38 @@ const PersonType = new GraphQLObjectType({
   },
 });
 
-const peopleData = [
-  { id: 1, name: 'John Smith' },
-  { id: 2, name: 'Sara Smith' },
-  { id: 3, name: 'Budd Deey' },
-];
+const peopleData = [];
+for (let pdIndex = 0; pdIndex < 26; pdIndex++) {
+  peopleData.push({id: pdIndex, name: String.fromCharCode("A".charCodeAt(0) + pdIndex)})
+}
 
 const QueryType = new GraphQLObjectType({
   name: 'Query',
   fields: {
     people: {
       type: new GraphQLList(PersonType),
-      resolve: () => peopleData,
+      args: {
+        order: {type: GraphQLString},
+        limit: {type: GraphQLInt},
+        page: {type: GraphQLInt},
+        explode: {type: GraphQLBoolean}
+      },
+      resolve: (_, args) => {
+        if (args.explode) {
+          throw new Error("Server \"down!\"");
+        }
+        const start = (args.page - 1) * args.limit;
+        const end = start + args.limit;
+        return [...peopleData].sort((a, b) => {
+          if (a.name === b.name) {
+            return 0;
+          }
+          if (a.name > b.name) {
+            return args.order === "asc" ? 1 : -1;
+          }
+          return args.order === "asc" ? -1 : 1;
+        }).slice(start, end);
+      }
     },
   },
 });
@@ -35,7 +57,7 @@ const MutationType = new GraphQLObjectType({
   fields: {
     addPerson: {
       type: PersonType,
-      args: { 
+      args: {
         name: { type: GraphQLString },
       },
       resolve: function (_, { name }) {
